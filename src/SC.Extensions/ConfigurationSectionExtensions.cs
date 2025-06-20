d3de05015e07e8111c9f279e30dbc7445f7c8c44
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-20 23:38:19 +0300

    Base Extensions

---
+++ src/SC.Extensions/ConfigurationSectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using SC.Abstraction;
+
+namespace SC.Extensions;
+
+public static class ConfigurationSectionExtensions
+{
+    public static T GetValue<T>(this IConfigurationSection section, string path) => section.GetValue<T>(new ConfigurationPath(path));
+
+    public static void SetValue<T>(this IConfigurationSection section, string path, T value) => section.SetValue(new ConfigurationPath(path), value);
+}
---
+++ src/SC.Newtonsoft.JSON/JsonConfigurationSectionExtensions.cs
@@ -0,0 +1,8 @@
+﻿using SC.Abstraction;
+
+namespace SC.Newtonsoft.JSON;
+
+public static class JsonConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AppendJsonFile(this IConfigurationBuilder builder, string filePath) => builder.AppendSource(new JsonFileConfigurationSource(filePath));
+}
