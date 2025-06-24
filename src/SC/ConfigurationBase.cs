391bd0357b4b7f53262a2ad741b010ffc4b3ce52
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-24 15:24:44 +0300

    Sections Cache

--- src/SC/ConfigurationBase.cs
+++ src/SC/ConfigurationBase.cs
@@ -1,10 +1,13 @@
 ﻿using SC.Abstraction;
 using System;
+using System.Collections.Generic;
 
 namespace SC;
 
 public abstract class ConfigurationBase(string name, IConfigurationOptions options) : IConfiguration
 {
+    private readonly Dictionary<string, IConfigurationSection> m_SectionsCache = [];
+
     public ConfigurationBase(string name) : this(name, DefaultConfigurationOptions.Default) { }
 
     public string Name => name;
@@ -15,7 +18,19 @@ public abstract class ConfigurationBase(string name, IConfigurationOptions optio
 
     public abstract bool HasValue(string fullPath);
 
-    public IConfigurationSection GetSection(string prefix) => HasSection(prefix) ? new ConfigurationSection(prefix, this) : null;
+    public IConfigurationSection GetSection(string prefix)
+    {
+        if(m_SectionsCache.TryGetValue(prefix, out var section)) return section;
+
+        if(HasSection(prefix))
+        {
+            section = new ConfigurationSection(prefix, this);
+            m_SectionsCache.Add(prefix, section);
+            return section;
+        }
+
+        return null;
+    }
 
     public abstract string GetValue(string fullPath);
 
