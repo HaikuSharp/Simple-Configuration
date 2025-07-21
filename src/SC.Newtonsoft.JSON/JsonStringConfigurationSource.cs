c785f667d2cd015de135837b6a54940ab07b975e
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-07-21 22:18:28 +0300

    Update json implementation

---
+++ src/SC.Newtonsoft.JSON/JsonStringConfigurationSource.cs
@@ -0,0 +1,8 @@
+﻿using SC.Abstraction;
+
+namespace SC.Newtonsoft.JSON;
+
+public sealed class JsonStringConfigurationSource(string name, string jsonString) : IConfigurationSource
+{
+    public IConfiguration Create(IConfigurationOptions options) => new FlattenConfiguration(name, options, JsonHelper.FlattenJson(jsonString, options.Separator));
+}
