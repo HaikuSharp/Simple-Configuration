0e73d4d17ea3d668dcecd4cf835495aa595315b7
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-08-11 21:51:18 +0300

    Create ConfigurationOptionExtensions.cs

---
+++ src/SC/Extensions/ConfigurationOptionExtensions.cs
@@ -0,0 +1,8 @@
+﻿using SC.Abstraction;
+
+namespace SC.Extensions;
+
+public static class ConfigurationOptionExtensions
+{
+    public static bool IsDirty(this IConfigurationOption option) => option.Version is not 0;
+}
