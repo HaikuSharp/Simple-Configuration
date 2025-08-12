71aa6856a49593d7b7c5aa25e7154f444ac63d9d
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-08-12 21:10:16 +0300

    Update ConfigurationExtensions.cs

--- src/SC/Extensions/ConfigurationExtensions.cs
+++ src/SC/Extensions/ConfigurationExtensions.cs
@@ -1,8 +1,17 @@
 ﻿using SC.Abstraction;
+using System.Threading.Tasks;
 
 namespace SC.Extensions;
 
 public static class ConfigurationExtensions
 {
     public static IConfigurationSection GetSection(this IConfiguration configuration, string path) => new ConfigurationSection(configuration, path);
+
+    public static void Save(this IConfiguration configuration) => configuration.Save(null);
+
+    public static void Load(this IConfiguration configuration) => configuration.Load(null);
+
+    public static async Task SaveAsync(this IConfiguration configuration) => await configuration.SaveAsync(null);
+
+    public static async Task LoadAsync(this IConfiguration configuration) => await configuration.LoadAsync(null);
 }
