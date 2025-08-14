67cd59b18f0c068ec6e7799e6408c3e182927c16
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-08-14 12:48:35 +0300

    Create ConfigurationRootExtensions.cs

---
+++ src/SC/Extensions/ConfigurationRootExtensions.cs
@@ -0,0 +1,8 @@
+﻿using SC.Abstraction;
+
+namespace SC.Extensions;
+
+public static class ConfigurationRootExtensions
+{
+    public static bool TryGetConfiguration(this IConfigurationRoot root, string name, out IConfiguration configuration) => (configuration = root.GetConfiguration(name)) is not null;
+}
