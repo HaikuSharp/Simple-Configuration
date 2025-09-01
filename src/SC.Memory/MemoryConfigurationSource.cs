2df543dc6ab4967d3f454b294ddddb880ad366ed
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-09-01 14:30:14 +0300

    public values sources types

--- src/SC.Memory/MemoryConfigurationSource.cs
+++ src/SC.Memory/MemoryConfigurationSource.cs
@@ -1,9 +1,5 @@
 ﻿using SC.Abstraction;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SC.Memory;
 
@@ -14,72 +10,4 @@ public class MemoryConfigurationSource(string name, IDictionary<string, object> 
 {
     /// <inheritdoc/>
     protected override IConfigurationValueSource GetValueSource(IConfigurationSettings settings) => new MemoryConfigurationValueSource(source);
-
-    private class MemoryConfigurationValueSource(IDictionary<string, object> source) : IConfigurationValueSource
-    {
-        private readonly IDictionary<string, object> m_Source = source;
-        private readonly Dictionary<string, object> m_Values = source.ToDictionary(k => k.Key, k => k.Value);
-
-        /// <inheritdoc/>
-        public bool HasRaw(string path) => m_Values.ContainsKey(path);
-
-        /// <inheritdoc/>
-        public IEnumerable<string> GetRawsNames(string path) => m_Values.Keys.Where(p => p.StartsWith(path));
-
-        /// <inheritdoc/>
-        public bool TryGetRaw<T>(string path, out T rawValue)
-        {
-            if(m_Values.TryGetValue(path, out object value))
-            {
-                rawValue = InternalConvertValue<T>(value);
-                return true;
-            }
-
-            rawValue = default;
-            return false;
-        }
-
-        /// <inheritdoc/>
-        public T GetRaw<T>(string path) => InternalConvertValue<T>(m_Values[path]);
-
-        /// <inheritdoc/>
-        public void SetRaw<T>(string path, T rawValue) => m_Values[path] = rawValue;
-
-        /// <inheritdoc/>
-        public void RemoveRaw(string path) => _ = m_Values.Remove(path);
-
-        private static T InternalConvertValue<T>(object sourceValue) => (T)InternalConvertValue(sourceValue, typeof(T));
-
-        private static object InternalConvertValue(object sourceValue, Type type)
-        {
-            var converter = TypeDescriptor.GetConverter(sourceValue.GetType());
-            return converter.CanConvertTo(type) ? converter.ConvertTo(sourceValue, type) : throw new InvalidCastException();
-        }
-
-        /// <inheritdoc/>
-        public void Save() => CopyValues(m_Source, m_Values);
-
-        /// <inheritdoc/>
-        public void Load() => CopyValues(m_Values, m_Source);
-
-        private static void CopyValues(IDictionary<string, object> values, IDictionary<string, object> source)
-        {
-            values.Clear();
-            foreach(var kvp in source) values[kvp.Key] = kvp.Value;
-        }
-
-        /// <inheritdoc/>
-        public Task SaveAsync()
-        {
-            Save();
-            return Task.CompletedTask;
-        }
-
-        /// <inheritdoc/>
-        public Task LoadAsync()
-        {
-            Load();
-            return Task.CompletedTask;
-        }
-    }
 }
---
+++ src/SC.Newtonsoft.JSON/JsonFileConfigurationValueSource.cs
@@ -0,0 +1,145 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SC.Abstraction;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SC.Newtonsoft.JSON;
+
+/// <summary>
+/// Represents a configuration values source that loads from and saves to JSON files.
+/// </summary>
+public class JsonFileConfigurationValueSource(string filePath, IConfigurationSettings settings) : IConfigurationValueSource
+{
+    private JToken m_Source;
+
+    private JToken NotNullSource => m_Source ??= new JObject();
+
+    /// <inheritdoc/>
+    public bool HasRaw(string path) => InternalGetRawJsonValue(path) is not null;
+
+    /// <inheritdoc/>
+    public IEnumerable<string> GetRawsNames(string path) => InternalGetRawJsonValue(path)?.Children().OfType<JProperty>().Select(p => p.Name) ?? [];
+
+    /// <inheritdoc/>
+    public bool TryGetRaw<T>(string path, out T rawValue)
+    {
+        var token = InternalGetRawJsonValue(path);
+
+        if(token is not null)
+        {
+            rawValue = token.ToObject<T>();
+            return true;
+        }
+
+        rawValue = default;
+        return false;
+    }
+
+    /// <inheritdoc/>
+    public T GetRaw<T>(string path)
+    {
+        var token = InternalGetRawJsonValue(path);
+        return token is not null ? token.ToObject<T>() : default;
+    }
+
+    /// <inheritdoc/>
+    public void SetRaw<T>(string path, T rawValue) => InternalGetOrCreateRawJsonValue(path).Replace(JToken.FromObject(rawValue));
+
+    /// <inheritdoc/>
+    public void RemoveRaw(string path) => InternalGetRawJsonValue(path)?.Remove();
+
+    private JToken InternalGetOrCreateRawJsonValue(string path)
+    {
+        var currentToken = NotNullSource;
+
+        if(currentToken is null) return null;
+        if(string.IsNullOrWhiteSpace(path)) return currentToken;
+        if(path.IndexOf(settings.Separator) is -1) return currentToken.SelectToken(path);
+
+        foreach(string pathPart in InternalGetPathEnumerator(path)) currentToken = currentToken.SelectToken(pathPart) ?? (currentToken[pathPart] = new JObject());
+
+        return currentToken;
+    }
+
+    private JToken InternalGetRawJsonValue(string path)
+    {
+        var currentToken = NotNullSource;
+
+        if(currentToken is null) return null;
+        if(string.IsNullOrWhiteSpace(path)) return currentToken;
+        if(path.IndexOf(settings.Separator) is -1) return currentToken.SelectToken(path);
+
+        foreach(string pathPart in InternalGetPathEnumerator(path))
+        {
+            currentToken = currentToken.SelectToken(pathPart);
+            if(currentToken is null) break;
+        }
+
+        return currentToken;
+    }
+
+    private ConfigurationPathEnumerator InternalGetPathEnumerator(string path) => new(path, settings.Separator);
+
+    /// <inheritdoc/>
+    public void Load()
+    {
+        if(!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+
+        using StreamReader streamReader = new(filePath);
+        using JsonTextReader jsonReader = new(streamReader);
+        m_Source = JToken.Load(jsonReader);
+    }
+
+    /// <inheritdoc/>
+    public void Save()
+    {
+        var source = NotNullSource;
+
+        string directory = Path.GetDirectoryName(filePath);
+
+        if(!string.IsNullOrEmpty(directory) && !Directory.Exists(directory)) _ = Directory.CreateDirectory(directory);
+
+        using StreamWriter streamWriter = new(filePath);
+        using JsonTextWriter jsonWriter = new(streamWriter)
+        {
+            Formatting = Formatting.Indented
+        };
+
+        source.WriteTo(jsonWriter);
+    }
+
+    /// <inheritdoc/>
+    public async Task LoadAsync()
+    {
+        if(!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+
+        using FileStream fileStream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 4096, useAsync: true);
+
+        using StreamReader streamReader = new(fileStream);
+        using JsonTextReader jsonReader = new(streamReader);
+        m_Source = await JToken.LoadAsync(jsonReader).ConfigureAwait(false);
+    }
+
+    /// <inheritdoc/>
+    public async Task SaveAsync()
+    {
+        if(m_Source == null) return;
+
+        string directory = Path.GetDirectoryName(filePath);
+
+        if(!string.IsNullOrEmpty(directory) && !Directory.Exists(directory)) _ = Directory.CreateDirectory(directory);
+
+        using FileStream fileStream = new(filePath, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true);
+
+        using StreamWriter streamWriter = new(fileStream);
+        using JsonTextWriter jsonWriter = new(streamWriter)
+        {
+            Formatting = Formatting.Indented
+        };
+
+        await m_Source.WriteToAsync(jsonWriter).ConfigureAwait(false);
+    }
+}
