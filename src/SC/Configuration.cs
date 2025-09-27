2219d246cdb8c01ab422b875a6fe6c35c73db415
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-09-27 19:28:29 +0300

    Fix GetOptionsNames

--- src/SC/Configuration.cs
+++ src/SC/Configuration.cs
@@ -26,7 +26,7 @@ public sealed class Configuration(IConfigurationSettings settings) : IConfigurat
     /// <inheritdoc/>
     public IEnumerable<string> GetOptionsNames(string path)
     {
-        var names = GetOptionsByPath(path).Select(e => e.Name);
+        var names = GetOptionsByPath(path).Select(e => e.Path.GetSectionName(path, Settings.Separator));
         if(TryGetLoadedSource(out var source)) names = names.Concat(source.GetRawsNames(path)).Distinct();
         return names;
     }
